50b0e838783e39c5405cbe53e1585883429eaa5c
izonejangwonyoung <2000shim@naver.com>
2023-07-17 17:28:51 +0900

    update patch note

--- src/component/UpdatePage/PatchNote.jsx
+++ src/component/UpdatePage/PatchNote.jsx
@@ -5,37 +5,33 @@ import Slider from 'react-slick'
 const cx = classNames.bind(styles)
 
 function PatchNote () {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    }
-
     return (
-        <div>
+        <div className={cx('changelog')}>
 
-            <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-            </Slider>
+            <h1 id="firebase_ios_release_notes" className={cx('page-title')}>FO4FORUM PATCH NOTE</h1>
+
+            <h2 id="_version_4130_april_13_2018"><a name="1.0.0"> Version 1.0.0 - <span className={cx('date')}>Sep 17, 2023</span></a></h2>
+
+            <h3 id="authentication">Publish</h3>
+            <ul>
+                <li><span className={cx('badge', 'badge-added')}>START</span> 실서버 운영 시작</li>
+            </ul>
+
+            {/* <ul> */}
+            {/*    <li><span className={cx('badge', 'badge-added')}>Confirmed</span> Resolved a crash which occurred when certain <span className={cx('quote')}>Firebase IDTokens</span> were */}
+            {/*        being parsed.</li> */}
+            {/* </ul> */}
+
+            {/* <h3 id="analytics">Analytics</h3> */}
+
+            {/* <ul> */}
+            {/*    <li><span className={cx('badge', 'badge-added')}>added</span> Added a new public method <code>resetAnalyticsData</code> to clear all */}
+            {/*        Analytics data as well as reset App Instance ID.</li> */}
+            {/*    <li><span className={cx('badge', 'badge-changed')}>changed</span> Improved In-App Purchase report: Analytics can report accurate */}
+            {/*        discounted price for introductory offer purchases.</li> */}
+            {/*    <li><span className={cx('badge', 'badge-fixed')}>fixed</span> Other bug fixes.</li> */}
+            {/* </ul> */}
+
         </div>
     )
 }
